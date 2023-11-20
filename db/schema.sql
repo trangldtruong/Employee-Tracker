8b0e42b0174df6aa8782c67a4d6749057ba9de81
Trang Truong <trangldtruong@gmail.com>
2023-11-20 10:41:45 -0500

    table fields added

--- db/schema.sql
+++ db/schema.sql
@@ -1,27 +1,27 @@
-DROP DATABASE IF EXISTS movies_db;
-CREATE DATABASE movies_db;
+DROP DATABASE IF EXISTS employees_db;
+CREATE DATABASE employees_db;
 
 USE employees_db;
 
 CREATE TABLE department (
-  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-  name VARCHAR(100) NOT NULL
+  id INT AUTO_INCREMENT PRIMARY KEY,
+  name VARCHAR(30) NOT NULL
 );
 
 CREATE TABLE role (
-    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    FOREIGN KEY (employee_id)
-    REFERENCES employee(id)
-    ON DELETE SET NULL
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    title VARCHAR(30),
+    salary DECIMAL,
+    department_id INT,
+    FOREIGN KEY (department_id) REFERENCES department(id) ON DELETE CASCADE
 );
 
 CREATE TABLE employee (
-    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    first_name INT,
-    last_name TEXT NOT NULL,
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    first_name VARCHAR(30),
+    last_name VARCHAR(30),
     role_id INT,
+    FOREIGN KEY (role_id) REFERENCES role (id) ON DELETE CASCADE,
     manager_id INT, 
-    FOREIGN KEY (employee_id)
-    REFERENCES employee(id)
-    ON DELETE SET NULL
+    FOREIGN KEY (manager_id) REFERENCES employee(id) ON DELETE SET NULL
 );
--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const { default: inquirer } = require('inquirer');
 const mysql = require('mysql2');
 
 const PORT = process.env.PORT || 3001;
@@ -20,3 +21,24 @@ const db = mysql.createConnection(
   },
   console.log(`Connected to the ... database`)
 );
+
+inquirer
+.prompt([
+    {
+        type: "list",
+        name: "userChoice",
+        message: "What would you like to do?",
+        choices: [
+          "View all departments",
+          "View all roles",
+          "View all employees",
+          "Add a department",
+          "Add a role",
+          "Add an employee",
+          "Update an employee role",
+        ],
+
+        type: 
+        name: 
+    }
+])
