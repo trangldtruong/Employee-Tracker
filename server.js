d7617758aa375ce209fb0ae768e5e4c041974257
Trang Truong <trangldtruong@gmail.com>
2023-11-28 14:41:48 -0500

    prompt questions completed

--- server.js
+++ server.js
@@ -9,9 +9,7 @@ const app = express();
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
-// Connect to database
-
-);
+// Tasks
 
 inquirer
 .prompt([
@@ -23,17 +21,37 @@ inquirer
             {
                 name: "View all employees",
                 value: "VIEW_EMPLOYEES"
-            }
-          "View all departments",
-          "View all roles",
-          "View all employees",
-          "Add a department",
-          "Add a role",
-          "Add an employee",
-          "Update an employee role",
-        ],
+            },
+            {
+                name: "Add Employee",
+                value: "ADD_EMPLOYEE"
+            },
+            {   name: "Update Employee Role",
+                value: "UPDATE_ROLE"
 
-         
+            },
+            {
+                name: "View All Roles ",
+                value: "VIEW_ROLES"
+            },
+            {
+                name: "Add a role",
+                value: "ADD_ROLE"
+            },
+            {
+                name: "View All Departments",
+                value: "VIEW_DEPARTMENTS"
+            },
+            {
+                name: "Add Department",
+                value: "ADD_DEPARTMENT"
+            },
+            {
+                name: "Quit",
+                value: "QUIT"
+            }
+        ]
+
     }
 ]).then(res => {
     let choice = res.choice
@@ -41,8 +59,124 @@ inquirer
         case "VIEW_EMPLOYEES":
             viewEmployees();
             break;
+        case "ADD_EMPLOYEE":
+            addEmployee();            
+            break;
+        case "UPDATE_ROLE":
+            updateRole();
+            break;
+        case "VIEW_ROLES":
+            viewRoles();
+            break;
+        case "ADD_ROLE":
+            addRole();
+            break;
+        case "VIEW_DEPARTMENTS":
+            viewDepartments();
+            break;
+        case "ADD_DEPARTMENT":
+            addDepartment();
+            break;
+        case "QUIT":
+            break;
     }
 })
 function viewEmployees(){
+    db.query("SELECT * FROM employee", function (err, results) {
+        if (err) throw err;
+        console.log(results);
+})}
+
+function addEmployee(){
+    inquirer
+    .prompt([
+        {
+            name: "first_name",
+            type: "input",
+            message: "What is the employee's first name?"
+        },
+        {
+            name: "last_name",
+            type: "input",
+            message: "What is the employee's last name?"
+        },
+        {
+            name: "role",
+            type: "list",
+            message: "What is the employee's role?",
+            choice: ["Accountant", "Legal Team Lead", "Lawyer", "Customer Service", "Sales Lead", "Salesperson", "Lead Engineer"]
+        },
+        {
+            name: "manager",
+            type: "list",
+            message: "Who is the employee's manager?",
+            choices: ["John Doe", "Mike Chan", "Ashley Rodriguez", "Kevin Tupik", "Kunal Singh", "Malia Brown"]
+        }
+    ])
+    
+}
+
+function updateRole(){
+    inquirer
+    .prompt([
+        {
+            name: "employee",
+            type: "list",
+            message: "Which employee's role do you want to update?",
+            choices: ["John Doe", "Mike Chan", "Ashley Rodriguez", "Kevin Tupik", "Kunal Singh", "Malia Brown", "Sarah Lourd", "Tom Allen", "Sam Kash"]
+        },
+        {
+            name: "role",
+            type: "list",
+            message: "Which role do you want to assign the selected employee?",
+            choices: ["Sales Lead", "Salesperson", "Lead Engineer", "Software Engineer", "Account Manager", "Accountant", "Legal Team Lead"]
+        }
+    ])
+
+}
+
+function viewRoles() {
+    db.query("SELECT * FROM role", function (err, results) {
+        if (err) throw err;
+        console.log(results);
+})};
+
+function addRole() {
+    inquirer
+    .prompt([
+        {
+        name: "name",
+        type: "input",
+        message: "What is the name of the role?"
+        },
+        {
+            name: "salary",
+            type: "input",
+            message: "What is the salary of the role?"
+        },
+        {
+            name: "department",
+            type: "list",
+            message: "Which department does the role belong to?",
+            choices: ["Engineering", "Finance", "Legal", "Sales", "Service"]
+        }
+    ])
+}
+
+function viewDepartments() {
+    db.query("SELECT * FROM department", function (err, results) {
+        if (err) throw err;
+        console.log(results);
+})};
+
+function addDepartment() {
+    inquirer
+    .prompt([
+        {
+            name: "name",
+            type: "input",
+            message: "What is the name of the department?"
+        }
+    ])
 
 }
